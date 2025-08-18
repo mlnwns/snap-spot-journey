370b213e13adc21b4b2dacbbfabcffd97f2fe2c5
hoyyChoi <hoyeon8165@gmail.com>
2025-08-19 03:19:53 +0900

    chore: ektl

--- frontend/src/hooks/use-mobile.tsx
+++ frontend/src/hooks/use-mobile.tsx
@@ -9,7 +9,7 @@ export function useIsMobile() {
 
   React.useEffect(() => {
     const mql = window.matchMedia(
-      `(max-wideth: ${MOBILE_BREAKPOINT - 1}pxeewefㅂㅈㄷㄹ4444wefwefewwe)`
+      `(max-wideth: ${MOBILE_BREAKPOINT - 1}pxeewefㅂㅈwerㄷㄹ4444wefwefewwe)`
     );
     eee;
     const onChange = () => {
